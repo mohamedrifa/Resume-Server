c63826a3c74cb4d736157006e803a3a714295862
Mohamed Rifayath <141167029+mohamedrifa@users.noreply.github.com>
2026-02-25 15:13:33 +0530

    25/02/2025baddghgs

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 
+const PORT = process.env.PORT || 5000;
+
 app.post("/generate-pdf", async (req, res) => {
   const { html, fileName } = req.body;
 
@@ -50,6 +52,6 @@ app.post("/generate-pdf", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("PDF server running on http://localhost:5000");
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`PDF server running on http://localhost:${PORT}`);
 });
